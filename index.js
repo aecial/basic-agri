06feb2ea93c0abfe3cddc7fd2f2aac8c3d06a9dc
Teddy Pascual <teddylayson2000@gmail.com>
2023-06-09 11:45:27 +0800

    created auction produce type cards

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ function login() {
   const username = document.getElementById("username_inp").value;
   const password = document.getElementById("password_inp").value;
   if (username && password === "admin") {
-    window.location.assign("HomePage.html");
+    window.location.assign("AuctionPage.html");
     alert("Welcome Admin!");
   }
+}
+
+function display(name) {
+  alert(`You clicked ${name}`);
 }
