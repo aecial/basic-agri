4e9b090613c0a324f8c966b235ad881805ca1db8
Teddy Pascual <teddylayson2000@gmail.com>
2023-06-10 20:01:47 +0800

    updated button interactivity but with errors

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const userLoggedIn = "Teddy";
+
 function login() {
   const username = document.getElementById("username_inp").value;
   const password = document.getElementById("password_inp").value;
@@ -10,4 +12,32 @@ function login() {
 function display(name) {
   alert(`You clicked ${name}`);
   window.location.assign(`${name}.html`);
+}
+
+function newAuction() {
+  window.location.assign("newAuction.html");
+}
+function newListing(category) {
+  window.location.assign(`${category}.html`);
+  const listings = document.getElementById("listings");
+  let newBtn = document.createElement("button");
+  newBtn.innerText = `${category} auction`;
+  newBtn.className = `${category}-newBtn`;
+  listings.appendChild(newBtn);
+}
+
+function confirmSubmit() {
+  let crop_category = document.getElementById("crop_category").value;
+  const crop_price = document.getElementById("crop_price").value;
+  const crop_volume = document.getElementById("crop_volume").value;
+  if (
+    confirm(
+      `Crop Category: ${crop_category}\nCrop Price per Kg: ${crop_price}\nCrop Volume: ${crop_volume}`
+    )
+  ) {
+    newListing(crop_category);
+    alert("created new listing");
+  } else {
+    alert("Sadge");
+  }
 }
